b895877a0e932e19469918f803e35d15df09e5bc
Eric McKevitt <erictmckevitt@gmail.com>
2025-04-06 02:16:14 -0600

    added spotify logo and open app on click

--- plugins/spotify.sh
+++ plugins/spotify.sh
@@ -1,9 +1,42 @@
   #!/bin/bash
-  if osascript -e 'application "Spotify" is running'; then
-    TRACK=$(osascript -e 'tell application "Spotify" to name of current track')
-    ARTIST=$(osascript -e 'tell application "Spotify" to artist of current track')
-    sketchybar --set $NAME label="$TRACK - $ARTIST"
+  #
+  # if osascript -e 'application "Spotify" is running'; then
+  #   TRACK=$(osascript -e 'tell application "Spotify" to name of current track')
+  #   ARTIST=$(osascript -e 'tell application "Spotify" to artist of current track')
+  #   sketchybar --set $NAME label="$TRACK - $ARTIST"
+  # else
+  #   sketchybar --set $NAME label="Spotify is not running"
+  # fi
+
+
+# if osascript -e 'application "Spotify" is running'; then
+#   TRACK=$(osascript -e 'tell application "Spotify" to name of current track')
+#   ARTIST=$(osascript -e 'tell application "Spotify" to artist of current track')
+#   STATE=$(osascript -e 'tell application "Spotify" to player state')
+#
+#   if [ "$STATE" = "playing" ]; then
+#     sketchybar --set spotify label="$TRACK - $ARTIST" drawing=on
+#   else
+#     sketchybar --set spotify drawing=off
+#   fi
+# else
+#   sketchybar --set spotify drawing=off
+# fi
+#
+
+#!/bin/bash
+
+if osascript -e 'application "Spotify" is running'; then
+  TRACK=$(osascript -e 'tell application "Spotify" to name of current track')
+  ARTIST=$(osascript -e 'tell application "Spotify" to artist of current track')
+  STATE=$(osascript -e 'tell application "Spotify" to player state')
+
+  if [ "$STATE" = "playing" ]; then
+    sketchybar --set spotify label="$TRACK - $ARTIST" drawing=on
   else
-    sketchybar --set $NAME label="Spotify is not running"
+    sketchybar --set spotify drawing=off
   fi
+else
+  sketchybar --set spotify drawing=off
+fi
 
