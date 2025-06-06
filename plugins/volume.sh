da947a09f2fa888f7e30d13148865f4cd7b2c6f7
Eric McKevitt <erictmckevitt@gmail.com>
2025-06-05 22:56:39 -0600

    volume item disappears when volume muted

--- plugins/volume.sh
+++ plugins/volume.sh
@@ -7,14 +7,15 @@ if [ "$SENDER" = "volume_change" ]; then
   VOLUME="$INFO"
 
   case "$VOLUME" in
-    [6-9][0-9]|100) ICON="󰕾"
-    ;;
-    [3-5][0-9]) ICON="󰖀"
-    ;;
-    [1-9]|[1-2][0-9]) ICON="󰕿"
-    ;;
-    *) ICON="󰖁"
+    [6-9][0-9]|100) ICON="󰕾" ;;
+    [3-5][0-9])     ICON="󰖀" ;;
+    [1-9]|[1-2][0-9]) ICON="󰕿" ;;
+    *)              ICON="󰖁" ;;  # This is the mute icon
   esac
 
-  sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%"
+  if [ "$VOLUME" -ne 0 ]; then
+    sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%" drawing=on
+  else
+    sketchybar --set "$NAME" drawing=off
+  fi
 fi
